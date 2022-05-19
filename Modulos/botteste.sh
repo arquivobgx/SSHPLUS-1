3bf9892dd438f422ea9ac588472be9e676b7b0b3
alfainternet <105681424+alfainternet@users.noreply.github.com>
2022-05-19 01:18:57 -0300

    Update botteste.sh

--- Modulos/botteste.sh
+++ Modulos/botteste.sh
@@ -1,62 +1,41 @@
 #!/bin/bash
-[[ $(screen -list| grep -c 'bot_teste') == '0' ]] && {
-    clear
-    echo -e "\E[44;1;37m     ATIVACÃO BOT SSH TESTE     \E[0m"
-    echo ""
-    echo -ne "\n\033[1;32mINFORME O TOKEN\033[1;37m: "
-    read token
-    clear
-    echo "-----------MODELO-----------"
-    echo "=×=×=×=×=×=×=×=×=×=×=×=×=×="
-    echo "   MENSAGEM DE BOAS VINDAS   "
-    echo "=×=×=×=×=×=×=×=×=×=×=×=×=×="
-    echo "        MENSAGEM FINAL         "
-    echo ""
-    echo -ne "\033[1;32mMENSAGEM DE BOAS VINDAS:\033[1;37m "
-    read bvindo
-    echo -ne "\033[1;32mMENSAGEM FINAL:\033[1;37m "
-    read mfinal
-    clear
-    echo -ne "\033[1;32mNOME DO BOTÃO 1(GERADOR SSH):\033[1;37m "
-    read bt1
-    clear
-    echo -ne "\033[1;32mNOME DO BOTÃO 2(PERSONALIZADO):\033[1;37m "
-    read bt2
-    echo -ne "\033[1;32mLINK DO BOTÃO 2:\033[1;37m "
-    read link2
-    clear
-    echo -ne "\033[1;32mNOME DO BOTÃO 3(PERSONALIZADO):\033[1;37m "
-    read bt3
-    echo -ne "\033[1;32mLINK DO BOTÃO 3:\033[1;37m "
-    read link3
-    clear
-    echo -ne "\033[1;32mDURAÇÃO DO TESTE(EM HORAS):\033[1;37m "
-    read dtempo
-    clear
-    echo ""
-    echo -e "\033[1;32mINICIANDO BOT TESTE \033[0m\n"
-    cd $HOME/BOT
-    rm -rf $HOME/BOT/botssh
-    wget https://www.dropbox.com/s/3aal16tw9jwixz8/botssh >/dev/null 2>&1
-    chmod 777 botssh
-    echo ""
-    sleep 1
-    sed -i "s/!#bvindo#!/$bvindo/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sed -i "s/!#mfinal#!/$mfinal/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sed -i "s/!#bt1#!/$bt1/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sed -i "s/!#bt2#!/$bt2/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sed -i "s/!#link2#!/$link2/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sed -i "s/!#bt3#!/$bt3/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sed -i "s/!#link3#!/$link3/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sed -i "s/!#dtempo#!/$dtempo/g" $HOME/BOT/botssh >/dev/null 2>&1
-    sleep 1
-    screen -dmS bot_teste ./botssh $token > /dev/null 2>&1
-    clear
-    echo "BOT ATIVADO"
-    menu
-} || {
-    screen -r -S "bot_teste" -X quit
-    clear
-    echo "BOT DESATIVADO"
-    menu
-}
+clear
+echo "America/Sao_Paulo" > /etc/timezone
+ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
+dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
+clear
+echo -e "\E[44;1;37m  🌟 INSTALAR CHECKUSER CONECTA4G 🌟   \E[0m" 
+echo ""
+echo -e "                 \033[1;31mBy @ALFAINTERNET\033[1;36m"
+echo ""
+echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
+clear
+echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
+apt-get install figlet -y > /dev/null 2>&1
+pip3 install flask > /dev/null 2>&1
+rm /bin/chuser > /dev/null 2>&1
+sleep 5
+cd /bin || exit
+wget https://github.com/alfainternet/SSHPLUS/raw/main/checkuser4g/chuser > /dev/null 2>&1
+wget https://github.com/alfainternet/SSHPLUS/raw/main/checkuser4g/user/userscheck > /dev/null 2>&1
+chmod 777 chuser > /dev/null 2>&1
+chmod 777 userscheck > /dev/null 2>&1
+clear
+mkdir /etc/rec > /dev/null 2>&1
+echo -e 'By: @ALFAINTERNET' > /etc/rec/licence
+echo -e 'By: @ALFAINTERNET' > /usr/lib/licence
+mkdir /usr/lib/checkuser > /dev/null 2>&1
+cd /usr/lib/checkuser || exit
+rm checkuser.py > /dev/null 2>&1
+wget https://github.com/alfainternet/SSHPLUS/raw/main/checkuser4g/checkuser.py > /dev/null 2>&1
+chmod 777 checkuser.py > /dev/null 2>&1
+clear
+echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
+sleep 2
+clear
+echo ""
+echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mchuser\033[0m"
+echo ""
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@ALFAINTERNET\033[0m"
+cat /dev/null > ~/.bash_history && history -c
+exit
